ecf6940ca56010026b85b700ef04983b1e160de8
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-08-19 21:48:37 +0300

    styled the header

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,18 +1,30 @@
-import { AiOutlineSearch } from "react-icons/ai"
+import {AiOutlineSearch} from "react-icons/ai"
+import "./index.css"
 
 const Header = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div className="header">
-            <div className="logo">BookRev</div>
-            <div className="searchBar">
-                <input type="text" placeholder="Search books" />
-                <AiOutlineSearch size={25} />
-            </div>
-            <div className="suggest-book">
-                <button>Suggest a book</button>
+            <div className="container">
+                <div className="logo">BookRev</div>
+                <div className="searchBar">
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Search books"/>
+                        <button type="submit">
+                            <AiOutlineSearch size={25}/>
+                        </button>
+                    </form>
+                </div>
+                <div className="suggest-book">
+                    <button>Suggest a book</button>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Header
